63c94730529d6f07746e5ce0c32db16fab9bf54e
shourya chauhan <shouryapschauhan@gmail.com>
2022-09-12 12:39:33 +0530

    firstcommit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", Output)
+	http.Handle("/", r)
+
+	http.ListenAndServe(":8080", r)
+}
+
+func Output(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Random number is: %v", RandomNumber())
+}
+
+func RandomNumber() int {
+	return rand.Intn(1000)
+}
